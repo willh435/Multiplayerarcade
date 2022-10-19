72254b81c0d80ae70af091de501a405415cae26e
jackwooo <115959001+jackwooo@users.noreply.github.com>
2022-10-19 12:39:54 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 3 3 3 3 3 . . . . . 
-    . . . . . 3 3 . 3 3 3 . . . . . 
-    . . . . . . 3 3 . . 3 3 . . . . 
-    . . . . . 3 . . . 3 3 . 3 . . . 
-    . . . . . 3 . . . 3 . . 3 . . . 
-    . . . . . 3 . . 3 3 . . . 3 . . 
-    . . . . . . 3 3 3 . . . . 3 . . 
-    . . . . . . . . . . . . . 3 . . 
-    . . . . . . . . . . . . . 3 . . 
-    . . . . . . . . . . . . . 3 . . 
-    . . . . . . . . . . . . 3 . . . 
-    . . . . . . . . . . . . 3 . . . 
-    . . . . . . . . . . . 3 . . . . 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f e e e e e f f e e e e e f f 
+    f f e e e e e f f e e e e e f f 
+    f f e e e e e f f e e e e e f f 
+    f f e e e e e f f e e e e e f f 
+    f f e e e e e f f e e e e e f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f e e e e e e e e f f f f 
+    f f f f e e e e e e e e f f f f 
+    f f f f e e e e e e e e f f f f 
+    f f f f e e e e e e e e f f f f 
+    f f f f e e e e e e e e f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
     `, SpriteKind.Player)
